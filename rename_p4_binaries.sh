f8f1d3e813545aca182b3d1371ad574170bdc4a2
Patrick M. Slattery <pslattery@mywebgrocer.com>
2014-03-13 18:43:52 -0400

    Don't rename if the file has already been renamed.

--- rename_p4_binaries.sh
+++ rename_p4_binaries.sh
@@ -10,7 +10,15 @@ do
   then 
     MAJOR_VERSION=$($f -V|grep "Rev."|cut -d'/' -f3|cut -d' ' -f1)
     MINOR_VERSION=$($f -V|grep "Rev."|cut -d'/' -f4|cut -d' ' -f1)
-    mv --verbose $f $f.$MAJOR_VERSION.$MINOR_VERSION
+    # Don't rename if the file has already been renamed.
+    # If the last chunk of the existing filename = $MINOR_VERSION then skip this file
+    if [ "$(ls $f|cut -d'.' -f4)" == "$MINOR_VERSION" ];
+    then
+      # print a null char, nothing appears on screen and it satisfies the requirement that we have a statement here
+     echo -en "\x00"
+    else
+      mv --verbose --interactive $f $f.$MAJOR_VERSION.$MINOR_VERSION
+    fi
   else
     echo "File '$f' is not executable"
   fi
